3d79b35c2a57983d55241bf068da295d29d34277
Prunera Santaularia <vprunera@indra.es>
2018-01-29 13:02:19 +0100

    first commit

---
+++ src/main/java/com/gastos/Gastos/GastosApplication.java
@@ -0,0 +1,12 @@
+package com.gastos.Gastos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastosApplication.class, args);
+	}
+}
